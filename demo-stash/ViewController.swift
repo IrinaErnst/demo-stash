d37fda425a472b116678797053d52dc8bf8ed490
IrinaErnst <IrinaKalashnikova@gmail.com>
2017-09-11 22:50:15 -0400

    Initial commit

---
+++ demo-stash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demo-stash
+//
+//  Created by Irina Kalashnikova on 9/11/17.
+//  Copyright © 2017 irinaernst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
